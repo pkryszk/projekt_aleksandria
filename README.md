d193b33446ea89d879e8b513b66b856da119868d
pkryszk <pkryszk-github@yahoo.com>
2023-06-24 14:37:03 +0200

    Fabryki

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ coverage run manage.py test
 coverage report
 coverage html
 start htmlcov/index.html
+nice interface python -m install ipython
+python manage.py shell
 
 Simple project to register users on courses
 
--- courses/tests/tests_models.py
+++ courses/tests/tests_models.py
@@ -1,51 +1,31 @@
 from django.test import TestCase
-from courses.models import Course, Review, Enrollment
-from django.contrib.auth.models import User
-from datetime import date
+
+from courses import factories
 
 
-class BaseTest(TestCase):
+class TestCourseModel(TestCase):
+
     def setUp(self):
-        self.user = User.objects.create_user(username="testuser", password="12345")
-        self.course = Course.objects.create(
-            title="Python",
-            description="Python course",
-            start_data=date(2023, 1, 1),
-            end_data=date(2023, 1, 10),
-            author=self.user,
-            price=100.00
-        )
-
-
-class TestCourseModel(BaseTest):
-
+        self.course = factories.CourseFactory(title="Python")
     def test_course_model_str_method(self):
         self.assertEqual(str(self.course), "Python")
 
 
-class TestReviewModel(BaseTest):
+class TestReviewModel(TestCase):
 
     def setUp(self):
-        super().setUp()
-        self.review = Review.objects.create(
-            user=self.user,
-            course=self.course,
-            rating=5,
-            comment="Great course"
-        )
+        self.review = factories.ReviewFactory()
 
     def test_review_model_str_method(self):
-        expected_str = f"{self.user.username} - {self.course.title} - {self.review.rating}"
+        expected_str = f"{self.review.user.username} - {self.review.course.title} - {self.review.rating}"
         self.assertEqual(str(self.review), expected_str)
 
-class TestEnrollmentModel(BaseTest):
+
+class TestEnrollmentModel(TestCase):
     def setUp(self):
-        super().setUp()
-        self.enrollment = Enrollment.objects.create(
-            user=self.user,
-            course=self.course
-        )
+
+        self.enrollment = factories.EnrollmentFactory()
 
     def test_enrollment_model_str_method(self):
-        expected_str = f"{self.user.username} - {self.course.title}"
+        expected_str = f"{self.enrollment.user.username} - {self.enrollment.course.title}"
         self.assertEqual(str(self.enrollment), expected_str)
